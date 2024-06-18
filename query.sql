02e27f85778c17e4e6ed7e3d6c3e08e8f87e2002
name <name@MacBook-Air--name.local>
2024-06-18 17:39:13 +0300

    upd sql

--- query.sql
+++ query.sql
@@ -1,3 +1,3 @@
-SELECT p.Title, c.Title from Products p
-    LEFT JOIN ProductsCategories pc on pc.ProductId = p.Id
-    LEFT JOIN Categories c on c.Id = pc.CategoryId
+SELECT p.title "Product", c.title "Category" FROM products p
+    LEFT JOIN product_categories pc ON pc.product_id = p.id
+    LEFT JOIN categories c ON c.id = pc.category_id;
